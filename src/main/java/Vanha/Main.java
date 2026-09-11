e5bed8100d9aafb8abb1f141e3cbe0e9ae07a39b
DanielVanha <daniel.vanha.s@gyarab.cz>
2026-09-11 08:54:02 +0200

    .

--- src/main/java/Vanha/Main.java
+++ src/main/java/Vanha/Main.java
@@ -28,7 +28,7 @@ public class Main {
             System.out.println(arr.get(prostredniIndex));
         }
         else {
-            int cislo1 = arr.get(prostredniIndex - 1);
+            int cislo1 = arr.get(prostredniIndex + 1);
             int cislo2 = arr.get(prostredniIndex);
             int median = (cislo1 + cislo2) / 2;
             System.out.println(median);
