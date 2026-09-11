106c8c5c16e894533c09866e47e1b3a0b8d4d9bc
Erik Zumr <erik.zumr.s@gyarab.cz>
2026-09-11 08:20:18 +0200

    Muj Commít

---
+++ src/main/java/org/example/zumr/najdiMedian.java
@@ -0,0 +1,19 @@
+package org.example.zumr;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class najdiMedian {
+    static void main(){
+
+        Scanner sc = new Scanner(System.in);
+        List<Integer> cisla = new ArrayList<>();
+            int a = sc.nextInt();
+
+            while(a!= 0){
+                a= sc.nextInt();
+            }
+
+    }
+}
