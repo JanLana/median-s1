bea3b0efce182f1abae62d594d2e84c83412d8fb
Erik Zumr <erik.zumr.s@gyarab.cz>
2026-09-11 09:00:21 +0200

    Muj Commít2

--- src/main/java/zumr/najdiMedian.java
+++ src/main/java/zumr/najdiMedian.java
@@ -1,6 +1,7 @@
 package zumr;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,8 +13,23 @@ public class najdiMedian {
             int a = sc.nextInt();
 
             while(a!= 0){
+                cisla.add(a);
                 a= sc.nextInt();
             }
+            Collections.sort(cisla);
+
+            double median;
+            int size = cisla.size();
+
+            if(size % 2 == 1){
+            median = cisla.get(size / 2);
+            }else{
+                median = (cisla.get((size / 2) - 1) + cisla.get(size / 2)) / 2.0;
+            }
+            System.out.println("Median je " + median);
+
+
+
 
     }
 }
